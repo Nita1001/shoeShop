4908621d073bfd3f7aebc58942358c21a458d08a
Nita1001 <devinedevops@gmail.com>
2023-02-25 16:12:58 +0200

    ProductsPage and Links

--- src/pages/Products.page.js
+++ src/pages/Products.page.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import classes from './Products.module.css'
+
+
+const PRODUCTS = [{id: 'p1', title: 'Product1'},{id: 'p2', title: 'Product2'},{id: 'p3', title: 'Product3'}];
+
 const Products = () => {
+
   return (
-    <>
+    <div className={classes.container}>
       <h3>Products</h3>
-      <ul>
-        <li><Link></Link></li>
-        <li><Link></Link></li>
-        <li><Link></Link></li>
-        <li><Link></Link></li>
+      <ul className={classes.list}>
+        {PRODUCTS.map(products => <Link to={`/products/${products.id}`} key={products.id}>{products.title}</Link>)}
       </ul>
-    </>
+    </div>
   )
 }
 
