2a5b5574ff3593c6cf6f59e27380a7d07fc3abbc
Nita1001 <devinedevops@gmail.com>
2023-02-25 14:50:25 +0200

    ErrorPage MainNav

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import HomePage from './pages/Home.page';
 import Products from './pages/Products.page';
-import RootLayout from './pages/Root'
+import RootLayout from './pages/Root';
+import ErrorPage from './pages/Error.page';
 
 import './App.css';
-
 
 const router = createBrowserRouter([
   { path: '/',
     element: <RootLayout />,
+    errorElement: <ErrorPage/>,
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <Products /> },
     ],
   },
 ])
-
 
 function App() {
   return (
