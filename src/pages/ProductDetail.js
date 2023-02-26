900fd24e91d2fdcc60361d33a8e88a0865f2ca37
Nita1001 <devinedevops@gmail.com>
2023-02-26 05:35:10 +0200

    displaying image of product from API

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,15 +1,55 @@
-import React from 'react'
-import { useParams, Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
+
 const ProductDetail = () => {
+    const [items, setItem] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const params = useParams();
+    const [selected, setSelected] = useState(); 
+
+    const fetchItem = async () => {
+        try {
+            const response = await axios.get(`https://63f7c3f2833c7c9c608aeae0.mockapi.io/items`);
+            if (response) {
+                setItem(response.data);
+                console.log(response.data)
+                setIsLoading(false);
+            }
+        } catch (err) {
+            console.log('status:', err);
+        }
+    };
+
+    useEffect(() => {
+        fetchItem();
+    }, []);
+
+useEffect(()=>{
+    setSelected ( items.find((item) => item.id === params.productId));
+    console.log(selected)
+}, [items, params.productId, selected])
 
     return (
         <>
-        <h3>Take bold steps</h3>
-        <p>{params.productId}</p>
-        <p><Link to=".." relative='path'>Back</Link></p>
+            <h3>Take bold steps</h3>
+            <p>{params.productId}</p>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : selected ? (
+                <div>
+                    <img src={selected.image} alt={selected.product} />
+                </div>
+            ) : (
+                <div>Item not found</div>
+            )}
+            <p>
+                <Link to=".." relative="path">
+                    Back
+                </Link>
+            </p>
         </>
-    )
-}
+    );
+};
 
-export default ProductDetail
+export default ProductDetail;
