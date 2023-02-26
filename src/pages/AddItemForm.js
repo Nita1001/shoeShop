2d790dce06f30f87cef2e0d6ac089e6c4925b392
Nita1001 <devinedevops@gmail.com>
2023-02-26 06:57:51 +0200

    removed Item successfully from API

--- src/pages/AddItemForm.js
+++ src/pages/AddItemForm.js
@@ -5,6 +5,10 @@ import uniqid from 'uniqid';
 import classes from './AddItemForm.module.css'
 
 const AddItemForm = () => {
+  const [add, setAdd] = useState(false);
+  const [remove, setRemove] = useState(false);
+  const [usersChoice, setUsersChoice] = useState(false);
+
   const [data, setData] = useState([]);
   const [product, setProduct] = useState('');
   const [price, setPrice] = useState('');
@@ -30,24 +34,63 @@ const AddItemForm = () => {
     console.log(data);
   };
 
+  const onRemoveItem = async () => {
+    try {
+      const response = await axios.delete(`https://63f7c3f2833c7c9c608aeae0.mockapi.io/items/${product}`);
+      if (response) setData(prevData => prevData.filter(item => item.id !== product));
+    } catch (err) {
+      console.log('status:', err);
+    }
+  };
+
+  const handleRemove = () => {
+    setUsersChoice(true);
+    setRemove(true);
+    setAdd(false);
+  };
+
+  const handleAdd = () => {
+    setUsersChoice(true);
+    setAdd(true);
+    setRemove(false);
+  };
+
   return (
-    <div className={classes.container}>CreateItem
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="product">Product:</label>
-          <input type="text" id="product" value={product} onChange={(e) => setProduct(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="price">Price:</label>
-          <input type="text" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
-        </div>
-        <div>
-          <label htmlFor="image">Image (URL):</label>
-          <input type="text" id="image" value={image} onChange={(e) => setImage(e.target.value)} />
-        </div>
-        <button type="submit">Add Item</button>
-      </form>
-    </div>
+    <>
+      {!usersChoice ?
+        <>
+          <button onClick={handleRemove}>remove Item</button>
+          <button onClick={handleAdd}>Add Item</button>
+        </>
+        : add ?
+          <div className={classes.container}>CreateItem
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="product">Product:</label>
+                <input type="text" id="product" value={product} onChange={(e) => setProduct(e.target.value)} />
+              </div>
+              <div>
+                <label htmlFor="price">Price:</label>
+                <input type="text" id="price" value={price} onChange={(e) => setPrice(e.target.value)} />
+              </div>
+              <div>
+                <label htmlFor="image">Image (URL):</label>
+                <input type="text" id="image" value={image} onChange={(e) => setImage(e.target.value)} />
+              </div>
+              <button type="submit">Add Item</button>
+            </form>
+          </div>
+          : remove ?
+            <form>
+              <div>
+                <label htmlFor="product">Products Id:</label>
+                <input type="text" id="product" value={product} onChange={(e) => setProduct(e.target.value)} />
+              </div>
+              <button onClick={onRemoveItem}>Remove Item</button>
+            </form>
+            : null
+      }
+    </>
   );
 };
 
